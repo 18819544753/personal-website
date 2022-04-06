7c3ab6c0a4947f22a809617ea0612c51ae88213d
zhushaorang <18819544753@163.com>
2022-04-06 15:54:56 +0800

    cdn更换为七牛

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import 'lib-flexible'
 import './assets/css/reset.css'
 // import './assets/css/common.css'
 Vue.config.productionTip = false
-Vue.prototype.$cdnUrl = "https://cdn.jsdelivr.net/gh/18819544753/personal-website@develop/src/assets/images"
+Vue.prototype.$cdnUrl = "http://cdn.chenxiaoman.top/assets/images" // 七牛cdn
+// Vue.prototype.$cdnUrl = "https://cdn.jsdelivr.net/gh/18819544753/personal-website@develop/src/assets/images" // jsdelivr cdn
 let homeScrollTop = 0;
 Vue.prototype.$homeScroll = homeScrollTop; // 全局滚动位置
 
